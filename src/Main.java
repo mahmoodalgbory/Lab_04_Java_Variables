d8b63a1daadbb3cbd4d91836a081c63b1872093f
Mahmood algbory <mahmoodalgbory@mahmoods-air.wifi.uc.edu>
2022-09-15 14:34:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 11;
+        int intOperandB = 34;
+
+        int intSum = intOperandA + intOperandB;
+
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+
+        // ,intProduct, intDifference, intQuotient, intModulo
+
+        double doubleOperandA = 34.7;
+        double doubleOperandB = 6.9;
+
+        double doubleSum = doubleOperandA + doubleOperandB;
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+    }
+}
